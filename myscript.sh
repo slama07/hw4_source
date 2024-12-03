4b677bd296687adb9f3e6fe5767a8e57cc42f765
slama0077 <slama6305@gmail.com>
2024-12-02 23:49:53 -0600

    script

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 module load openmpi/4.1.4-gcc11
 mpicc -g -Wall -std=c99 -o hw4 hw4.c
-mpirun -n 1 hw4 5000 sonam_output11.txt
+mpirun -n 20 hw4 5000 sonam_output11.txt
 # mpirun -n 1 hw4 5000 sonam_output12.txt
 # mpirun -n 1 hw4 5000 sonam_output13.txt
 # mpirun -n 2 hw4 5000 sonam_output21.txt
